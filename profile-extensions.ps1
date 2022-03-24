501610700e61c6d3374df940628f510d432e3344
reaching-for-libra <reaching-for-libra@users.noreply.github.com>
2022-03-24 09:12:56 -0500

    Update profile-extensions.ps1

--- profile-extensions.ps1
+++ profile-extensions.ps1
@@ -1,7 +1,4 @@
 
-update-typedata -typename 'string' -membertype scriptmethod -membername FromJson -value {
-    $this | convertfrom-json
-} -force -confirm:$false 
 
 update-typedata -typename 'System.Object' -membertype scriptmethod -membername ToJson -value {
     param(
@@ -9,6 +6,14 @@ update-typedata -typename 'System.Object' -membertype scriptmethod -membername T
         [switch]$Compress)
     $this | convertto-json -depth $depth -compress:$compress
 } -force -confirm:$false 
+
+update-typedata -typename 'System.Management.Automation.PSCustomObject' -membertype scriptmethod -membername ToJson -value {
+    param(
+        [int]$Depth = 10,
+        [switch]$Compress)
+    $this | convertto-json -depth $depth -compress:$compress
+} -force -confirm:$false 
+
 
 update-typedata -typename 'System.Object' -membertype scriptmethod -membername With -value {
     param([hashtable]$props)
@@ -24,6 +29,23 @@ update-typedata -typename 'System.Object' -membertype scriptmethod -membername W
     write-output $copy
 } -force -confirm:$false 
 
+update-typedata -typename 'System.Management.Automation.PSCustomObject' -membertype scriptmethod -membername With -value {
+    param([hashtable]$props)
+    $copy = $this | select-object -property *
+    foreach($key in $props.keys){
+        if ($key -in $copy.psobject.properties.name){
+            $copy.${key} = $props[$key]
+        }else{
+            write-warning "Adding new property: $key"
+            $copy | add-member -notepropertyname $key -notepropertyvalue $props[$key]
+        }
+    }
+    write-output $copy
+} -force -confirm:$false 
+
+update-typedata -typename 'string' -membertype scriptmethod -membername FromJson -value {
+    $this | convertfrom-json
+} -force -confirm:$false 
 
 update-typedata -typename 'array' -membertype scriptmethod -membername ToJson -value {param([int]$Depth=3,[switch]$Compress)$this|convertto-json -depth $depth -compress:$compress} -force -confirm:$false 
 
