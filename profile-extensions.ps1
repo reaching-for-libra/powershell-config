97c666a8639b63720a4f06860ea85edc6959cf40
reaching-for-libra <reaching-for-libra@users.noreply.github.com>
2022-03-24 09:05:18 -0500

    Update profile-extensions.ps1

--- profile-extensions.ps1
+++ profile-extensions.ps1
@@ -3,28 +3,21 @@ update-typedata -typename 'string' -membertype scriptmethod -membername FromJson
     $this | convertfrom-json
 } -force -confirm:$false 
 
-update-typedata -typename 'psobject' -membertype scriptmethod -membername ToJson -value {
+update-typedata -typename 'System.Object' -membertype scriptmethod -membername ToJson -value {
     param(
         [int]$Depth = 10,
         [switch]$Compress)
     $this | convertto-json -depth $depth -compress:$compress
 } -force -confirm:$false 
 
-update-typedata -typename 'System.Management.Automation.PSCustomObject' -membertype scriptmethod -membername ToJson -value {
-    param(
-        [int]$Depth = 10,
-        [switch]$Compress)
-    $this | convertto-json -depth $depth -compress:$compress
-} -force -confirm:$false 
-
-update-typedata -typename 'System.Management.Automation.PSCustomObject' -membertype scriptmethod -membername With -value {
+update-typedata -typename 'System.Object' -membertype scriptmethod -membername With -value {
     param([hashtable]$props)
     $copy = $this | select-object -property *
     foreach($key in $props.keys){
         if ($key -in $copy.psobject.properties.name){
             $copy.${key} = $props[$key]
         }else{
-            write-warning "Adding new property $key"
+            write-warning "Adding new property: $key"
             $copy | add-member -notepropertyname $key -notepropertyvalue $props[$key]
         }
     }
