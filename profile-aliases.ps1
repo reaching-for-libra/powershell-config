b99e6df4736ff9f45de8d8a3ef2a7bd57382a903
reaching-for-libra <reaching-for-libra@users.noreply.github.com>
2022-03-28 09:39:16 -0500

    Update profile-aliases.ps1

--- profile-aliases.ps1
+++ profile-aliases.ps1
@@ -13,7 +13,62 @@ $aliases = @{
     which = 'get-command'
 
     #custom functions
-    split = profile-module\split-string
+    split = 'profile-module\split-string'
+
+    #queryhub
+    OAdd = 'queryhub\Add-OracleQueryHubConnection'
+    OBuild = 'queryhub\Invoke-OraclePackageBuild'
+    OCode = 'queryhub\Get-OracleCodeDefinition'
+    OCodes = 'queryhub\Get-OracleCodeNames'
+    OConnections = 'queryhub\Get-OracleQueryHubConnections'
+    ODef = 'queryhub\Get-OracleDefinition'
+    ODefault = 'queryhub\Get-OracleDefaultQueryHubConnection'
+    ODependencies = 'queryhub\Get-OracleDependencies'
+    OErrors = 'queryhub\Get-OracleErrors'
+    OExplain = 'queryhub\Get-OracleExplainPlan'
+    OFile = 'queryhub\Invoke-OracleQueryFromFile'
+    OHistory = 'queryhub\Get-OracleSessionSqlHistory'
+    OIndex = 'queryhub\Get-OracleIndex'
+    OKeys = 'queryhub\Get-OracleFndTableKeys'
+    OKillSession = 'queryhub\Stop-OracleSession'
+    OLastDbms = 'queryhub\Get-LastOracleDbmsOutput'
+    OLastError = 'queryhub\Get-LastOracleQueryError'
+    OLastExplain = 'queryhub\Get-LastOracleQueryExplain'
+    OLastRequest = 'queryhub\Get-LastOracleQueryRequest'
+    OLastTime = 'queryhub\Get-LastOracleQueryTime'
+    ORemove = 'queryhub\Remove-OracleQueryHubConnection'
+    OSelect = 'queryhub\Get-OracleTableSelectString'
+    OSession = 'queryhub\Get-OracleSession'
+    OSet = 'queryhub\Set-OracleDefaultQueryHubConnection'
+    OSetDate = 'queryhub\Set-OracleSessionDateFormat'
+    OSql = 'queryhub\Invoke-OracleQuery'
+    OTable = 'queryhub\Get-OracleTableSchema'
+    OTables = 'queryhub\Get-OracleTableNames'
+    OTest = 'queryhub\Test-OracleConnection'
+    os = 'queryhub\invoke-oraclescript'
+    oslist = 'queryhub\get-oraclescripts'
+
+    SAdd = 'queryhub\Add-SqlServerQueryHubConnection'
+    SCode = 'queryhub\Get-SqlServerDefinition'
+    SCodes = 'queryhub\Get-SqlServerCodeNames'
+    SConnections = 'queryhub\Get-SqlServerQueryHubConnections'
+    SDef = 'queryhub\Get-SqlServerDefinition'
+    SGetDefault = 'queryhub\Get-SqlServerDefaultQueryHubConnection'
+    SJobs = 'queryhub\Get-SqlServerJobs'
+    SLastMessage = 'queryhub\Get-LastSqlServerMessages'
+    SLastRequest = 'queryhub\Get-LastSqlServerQueryRequest'
+    SLastTime = 'queryhub\Get-LastSqlServerQueryTime'
+    SRemove = 'queryhub\Remove-SqlServerQueryHubConnection'
+    SSession = 'queryhub\Get-SqlServerSession'
+    SSet = 'queryhub\Set-SqlServerDefaultQueryHubConnection'
+    SSql = 'queryhub\Invoke-SqlServerQuery'
+    STable = 'queryhub\Get-SqlServerTableSchema'
+    STables = 'queryhub\Get-SqlServerTableNames'
+    STest = 'queryhub\Test-SqlServerConnection'
+    sLastError = 'queryhub\Get-LastSqlServerQueryError'
+    sfile = 'queryhub\Invoke-sqlserverQueryFromFile'
+    ss = 'queryhub\invoke-sqlserverscript'
+    sslist = 'queryhub\get-sqlserverscripts'
 }
 
 
@@ -21,72 +76,11 @@ foreach ($key in $aliases.keys){
     if (test-path "alias:$key"){
         remove-item "alias:$key" -confirm:$false -force
     }
+    if (-not (get-command $aliases[$key])){
+        write-warning "alias '$key' skipped because command '$($aliases[$key])' doesn't exist"
+        continue
+    }
     new-alias -name $key -value $aliases[$key] -confirm:$false
-}
-
-if ((get-module queryhub)){
-    $aliases = @{
-        OAdd = queryhub\Add-OracleQueryHubConnection
-        OBuild = queryhub\Invoke-OraclePackageBuild
-        OCode = queryhub\Get-OracleCodeDefinition
-        OCodes = queryhub\Get-OracleCodeNames
-        OConnections = queryhub\Get-OracleQueryHubConnections
-        ODef = queryhub\Get-OracleDefinition
-        ODefault = queryhub\Get-OracleDefaultQueryHubConnection
-        ODependencies = queryhub\Get-OracleDependencies
-        OErrors = queryhub\Get-OracleErrors
-        OExplain = queryhub\Get-OracleExplainPlan
-        OFile = queryhub\Invoke-OracleQueryFromFile
-        OHistory = queryhub\Get-OracleSessionSqlHistory
-        OIndex = queryhub\Get-OracleIndex
-        OKeys = queryhub\Get-OracleFndTableKeys
-        OKillSession = queryhub\Stop-OracleSession
-        OLastDbms = queryhub\Get-LastOracleDbmsOutput
-        OLastError = queryhub\Get-LastOracleQueryError
-        OLastExplain = queryhub\Get-LastOracleQueryExplain
-        OLastRequest = queryhub\Get-LastOracleQueryRequest
-        OLastTime = queryhub\Get-LastOracleQueryTime
-        ORemove = queryhub\Remove-OracleQueryHubConnection
-        OSelect = queryhub\Get-OracleTableSelectString
-        OSession = queryhub\Get-OracleSession
-        OSet = queryhub\Set-OracleDefaultQueryHubConnection
-        OSetDate = queryhub\Set-OracleSessionDateFormat
-        OSql = queryhub\Invoke-OracleQuery
-        OTable = queryhub\Get-OracleTableSchema
-        OTables = queryhub\Get-OracleTableNames
-        OTest = queryhub\Test-OracleConnection
-        os = queryhub\invoke-oraclescript
-        oslist = queryhub\get-oraclescripts
-
-        SAdd = queryhub\Add-SqlServerQueryHubConnection
-        SCode = queryhub\Get-SqlServerDefinition
-        SCodes = queryhub\Get-SqlServerCodeNames
-        SConnections = queryhub\Get-SqlServerQueryHubConnections
-        SDef = queryhub\Get-SqlServerDefinition
-        SGetDefault = queryhub\Get-SqlServerDefaultQueryHubConnection
-        SJobs = queryhub\Get-SqlServerJobs
-        SLastMessage = queryhub\Get-LastSqlServerMessages
-        SLastRequest = queryhub\Get-LastSqlServerQueryRequest
-        SLastTime = queryhub\Get-LastSqlServerQueryTime
-        SRemove = queryhub\Remove-SqlServerQueryHubConnection
-        SSession = queryhub\Get-SqlServerSession
-        SSet = queryhub\Set-SqlServerDefaultQueryHubConnection
-        SSql = queryhub\Invoke-SqlServerQuery
-        STable = queryhub\Get-SqlServerTableSchema
-        STables = queryhub\Get-SqlServerTableNames
-        STest = queryhub\Test-SqlServerConnection
-        sLastError = queryhub\Get-LastSqlServerQueryError
-        sfile = queryhub\Invoke-sqlserverQueryFromFile
-        ss = queryhub\invoke-sqlserverscript
-        sslist = queryhub\get-sqlserverscripts
-    }
-
-    foreach ($key in $aliases.keys){
-        if (test-path "alias:$key"){
-            remove-item "alias:$key" -confirm:$false -force
-        }
-        new-alias -name $key -value $aliases[$key] -confirm:$false
-    }
 }
 
 #automatic variables
