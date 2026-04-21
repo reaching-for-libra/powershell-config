6ea7cf7136dfee61f36a8153a6c18137b4437c15
reaching-for-libra <reaching-for-libra@users.noreply.github.com>
2026-04-21 09:27:33 -0500

    Update profile-module.psm1

--- profile-module.psm1
+++ profile-module.psm1
@@ -2034,3 +2034,156 @@ function get-responsevariablescope{
         write-output 2
     }
 }
+
+function Format-FlatObject{
+    param(
+        [parameter(valuefrompipeline,mandatory)]
+        [object]$FlatObject,
+
+        [switch]$WithColor,
+        [switch]$IncludeValue,
+        [switch]$Sort
+    )
+
+    process{
+        $gm = $flatobject.psobject.properties
+        if ($sort){
+            $gm = $gm | sort name
+        }
+        if (-not $withcolor){
+            $green = ""
+            $yellow = ""
+            $brightyellow = ""
+            $white = ""
+            $cyan = ""
+            $brightblack = ""
+            $reset = ""
+        }else{
+            $green = $psstyle.foreground.green
+            $yellow = $psstyle.foreground.yellow
+            $brightyellow = $psstyle.foreground.brightyellow
+            $white = $psstyle.foreground.white
+            $cyan = $psstyle.foreground.cyan
+            $brightblack = $psstyle.foreground.brightblack
+            $reset = $psstyle.reset
+        }
+        foreach($key in $gm.name){
+
+            if (-not $includevalue){
+                $value = ""
+            }else{
+                $value = "$yellow = $brightblack$($flatobject.$key)"
+            }
+            "$green$key$value$reset"
+        }
+    }
+}
+
+Function ConvertTo-FlatObject {
+    <#
+    .SYNOPSIS
+    Flattens a nested object into a single level object.
+    .DESCRIPTION
+    Flattens a nested object into a single level object.
+    .PARAMETER Objects
+    The object (or objects) to be flatten.
+    .PARAMETER Separator
+    The separator used between the recursive property names
+    .PARAMETER Base
+    The first index name of an embedded array:
+    - 1, arrays will be 1 based: <Parent>.1, <Parent>.2, <Parent>.3, …
+    - 0, arrays will be 0 based: <Parent>.0, <Parent>.1, <Parent>.2, …
+    - "", the first item in an array will be unnamed and than followed with 1: <Parent>, <Parent>.1, <Parent>.2, …
+    .PARAMETER Depth
+    The maximal depth of flattening a recursive property.
+    .NOTES
+    From https://evotec.xyz/powershell-converting-advanced-object-to-flat-object/
+    Based on https://powersnippets.com/convertto-flatobject/
+    #>
+    [CmdletBinding()]
+    Param (
+        [Parameter(ValueFromPipeLine)][Object[]]$Objects,
+        [String]$Separator = ".",
+
+        [ValidateSet("", 0, 1)]$Base = 0,
+
+        [Parameter(Mandatory = $false)]
+        [ValidateScript({ $_ -ge 0 })]
+        [int]
+        $Depth = 10,
+
+        [Parameter(DontShow)][String[]]$Path,
+        [Parameter(DontShow)][System.Collections.IDictionary] $OutputObject,
+
+        [alias('c')]
+        [switch]
+        $WithColor,
+
+        [alias('v')]
+        [switch]
+        $IncludeValue,
+
+        [alias('s')]
+        [switch]$Sort
+
+
+
+    )
+    Begin {
+        $InputObjects = [System.Collections.Generic.List[Object]]::new()
+    }
+    Process {
+        foreach ($O in $Objects) {
+            $InputObjects.Add($O)
+        }
+    }
+    End {
+        If ($PSBoundParameters.ContainsKey("OutputObject")) {
+            $Object = $InputObjects[0]
+            $Iterate = [ordered] @{}
+            if ($null -eq $Object) {
+                #Write-Verbose -Message "ConvertTo-FlatObject - Object is null"
+#            } elseif ($Object.GetType().Name -in 'String', 'DateTime', 'TimeSpan', 'Version', 'Enum') {
+           } elseif ($Object.GetType().Name -in 'String', 'DateTime', 'DateTimeOffset', 'TimeSpan', 'Version', 'Enum') {
+                $Object = $Object.ToString()
+            }
+            elseif ($Depth) {
+                $Depth--
+                If ($Object -is [System.Collections.IDictionary]) {
+                    $Iterate = $Object
+                }
+                elseif ($Object -is [Array] -or $Object -is [System.Collections.IEnumerable]) {
+                    $i = $Base
+                    foreach ($Item in $Object.GetEnumerator()) {
+                        $Iterate["[$i]"] = $Item
+                        $i += 1
+                    }
+                }
+                else {
+                    foreach ($Prop in $Object.PSObject.Properties) {
+                        if ($Prop.IsGettable) {
+                            $Iterate["$($Prop.Name)"] = $Object."$($Prop.Name)"
+                        }
+                    }
+                }
+            }
+            If ($Iterate.Keys.Count) {
+                foreach ($Key in $Iterate.Keys) {
+                    ConvertTo-FlatObject -Objects @(, $Iterate["$Key"]) -Separator $Separator -Base $Base -Depth $Depth -Path ($Path + $Key) -OutputObject $OutputObject  -withcolor:$withcolor -includevalue:$includevalue -sort:$sort
+                }
+            }
+            else {
+                $Property = $Path -Join $Separator
+$Property = $property -replace "$([regex]::escape($separator))\[",'['
+                $OutputObject[$Property] = $Object
+            }
+        }
+        elseif ($InputObjects.Count -gt 0) {
+            foreach ($ItemObject in $InputObjects) {
+                $OutputObject = [ordered]@{}
+                ConvertTo-FlatObject -Objects @(, $ItemObject) -Separator $Separator -Base $Base -Depth $Depth -Path $Path -OutputObject $OutputObject  -withcolor:$withcolor -includevalue:$includevalue -sort:$sort
+                [PSCustomObject] $OutputObject | format-flatobject -withcolor:$withcolor -includevalue:$includevalue -sort:$sort
+            }
+        }
+    }
+}
